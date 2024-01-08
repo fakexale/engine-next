891a351c262252bd33b4835776dbe28e26749aba
fakexale <thexalee@gmail.com>
2024-01-08 13:24:42 +0100

    Add Runtime:BindHeartbeatEvery()

--- src/Lib/Runtime.lua
+++ src/Lib/Runtime.lua
@@ -35,4 +35,23 @@ function Runtime:GetSettings(): RuntimeSettings
 	return self["RuntimeSettings"]
 end
 
+function Runtime:BindHeartbeatEvery(ticks: number, fn: () -> ()): () -> ()
+	local currentTick = 0
+
+	local connection = RunService.Heartbeat:Connect(function()
+		currentTick += 1
+
+		if currentTick >= ticks then 
+			currentTick = 0 
+			return
+		end
+
+		fn()
+	end)
+
+	return function()
+		connection:Disconnect()
+	end
+end
+
 return table.freeze(Runtime)
