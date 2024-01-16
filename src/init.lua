9e85e573a2302f7728219a3d12411877105d7732
xale <thexalee@gmail.com>
2024-01-16 17:50:16 +0000

    Shorten warning when requiring

--- src/init.lua
+++ src/init.lua
@@ -1,8 +1,6 @@
 local RunService = game:GetService("RunService")
 
-warn(
-	`This version of Engine3 is uncomplete, things WILL break, such as, but not limited to:\n\tEngineClient being empty\n\tRuntime, along with it's properties are untested\nThanks!`
-)
+warn("Engine3 is unfinished, but is still usable excluding EngineClient.")
 
 if RunService:IsServer() then
 	return require(script.EngineServer)
