2ee9dc93eeb8567608a5b386d286d818d8092d35
xale <thexalee@gmail.com>
2024-01-25 13:30:56 +0100

    Document Loader

--- src/Lib/Loader.lua
+++ src/Lib/Loader.lua
@@ -1,5 +1,20 @@
+--[=[
+	@class Loader
+
+	A utillity module used for loading ModuleScripts.
+]=]--
 local Loader = {}
 
+--[=[
+	Loads the children of the given Instance
+
+	```lua
+	Loader.LoadChildren(modules)
+
+	-- See Loader.MatchName() for how this works.
+	Loader.LoadChildren(modules, Loader.MatchName("Service$"))
+	```
+]=]--
 function Loader.LoadChildren(path: Instance, predicate: (module: ModuleScript) -> boolean?): { [string]: any }
 	local loaded: { [string]: any } = {}
 
@@ -19,6 +34,17 @@ function Loader.LoadChildren(path: Instance, predicate: (module: ModuleScript) -
 	return loaded
 end
 
+
+--[=[
+	Loads the descendants of the given Instance
+
+	```lua
+	Loader.LoadDescendants(modules)
+
+	-- See Loader.MatchName() for how this works.
+	Loader.LoadDescendants(modules, Loader.MatchName("Service$"))
+	```
+]=]--
 function Loader.LoadDescendants(path: Instance, predicate: (module: ModuleScript) -> boolean?): { [string]: any }
 	local loaded: { [string]: any } = {}
 
@@ -38,6 +64,19 @@ function Loader.LoadDescendants(path: Instance, predicate: (module: ModuleScript
 	return loaded
 end
 
+
+--[=[
+	Spawn the ``methodName`` of the given ``loadedModules`` returned by ``LoadChildren`` and ``LoadDescendants``.
+
+	Methods are called with ``task.spawn``.
+
+	```lua
+	local LoadedModules = Loader.LoadChildren(modules)
+
+	-- If function "Init", if it exists.
+	Loader.SpawnAll(LoadedModules, "Init")
+	```
+]=]--
 function Loader.SpawnAll(loadedModules: { [string]: any }, methodName: string)
 	for moduleName, module in loadedModules do
 		local method = module[methodName]
@@ -53,6 +92,14 @@ function Loader.SpawnAll(loadedModules: { [string]: any }, methodName: string)
 	end
 end
 
+--[=[
+	A function act as the predicate for ``LoadChildren`` and ``LoadDescendants``.
+
+	```lua
+	-- Loads children, if Loader.MatchName() resolves to true.
+	Loader.LoadChildren(modules, Loader.MatchName("Service$"))
+	```
+]=]--
 function Loader.MatchName(match: string): (module: ModuleScript) -> boolean
 	return function(moduleScript: ModuleScript): boolean
 		return string.match(moduleScript.Name, match) ~= nil
